1eefd4c97decb4e6d985c4430a398a388c6f4431
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-03-26 10:20:39 +0530

    array merge is done

---
+++ include/mergeArray.php
@@ -0,0 +1,6 @@
+<?php
+function mergeArray($array,$array2){
+    $array3 = array_merge($array,$array2);
+    return $array3;
+}
+
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
  <?php
 include 'include/arraySum.php';
 include 'include/maxValArray.php';
+include 'include/mergeArray.php';
 $array = [1, 2, 3, 4, 5, 6, 7, 8];
+$array2 = [9, 10, 11, 12, 13, 14, 15, 16];
+
 echo sumArray($array);
 echo "<br>";
 echo maxVal($array);
+echo "<br>";
+print_r(mergeArray($array, $array2));
