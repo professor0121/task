41f062a7517ed82362eb57417b59994ee45893dc
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-03-26 10:14:40 +0530

    max value from array is done

--- include/maxValArray.php
+++ include/maxValArray.php
@@ -1,2 +1,12 @@
 <?php
+function maxVal($array){
+    $max=$array[0];
+    foreach ($array as $key => $value) {
+        if($value>$max){
+            $max=$value;
+        }
+    }
+    return $max;
+}
 
+ 
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
  <?php
 include 'include/arraySum.php';
+include 'include/maxValArray.php';
 $array = [1, 2, 3, 4, 5, 6, 7, 8];
 echo sumArray($array);
+echo "<br>";
+echo maxVal($array);
